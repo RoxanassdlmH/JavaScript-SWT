1aaeb1aea8c8dbe0a991cad72208147f7e28f9ab
Roxanassdlm <ada_hack220@hackademy.mx>
2022-03-28 14:25:19 -0700

    JavaScript 3 Changes Solved

---
+++ Script3.js
@@ -0,0 +1,25 @@
+let userName =''
+userName? console.log(`Hello, ${userName}!`):console.log('Hello!');
+let textInt= 'Please select an animal:'
+let arr=['0:Dog','1:Cat','2:Exit']
+console.log(textInt)
+console.log(arr)
+let  decision=''
+
+if (decision==0){
+  console.log(' \"_=,_\r\n  o_\/6 \/#\\\r\n  \\__ |##\/\r\n   =\'|--\\\r\n     \/   #\'-.\r\n     \\#|_   _\'-. \/\r\n      |\/ \\_( # |\r\n     C\/ ,--___\/'
+);
+} else if(decision==1){
+  console.log('    |\\__\/,|   (`\\\r\n  _.|o o  |_   ) )\r\n-(((---(((--------');
+} else if (decision==2){
+  console.log('Game over')
+} else { 
+  console.log('Input your choice')
+}
+
+
+
+//* let userQuestion=' Will I become a werewolf tonight?'
+// userQuestion? console.log(`The ${userName} asked${userQuestion}`):console.log('Dont forget to ask your question!');
+//const randomNumber= Math.floor(Math.random()*8);
+// let eightball=''
